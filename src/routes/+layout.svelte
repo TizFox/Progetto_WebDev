8be1651d54a2bb506b54c4c8c81247329fe9c558
TizFox <tizbona04@gmail.com>
2026-02-17 10:35:38 +0100

    added navbar, footer, items

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,71 @@
 <script lang="ts">
-	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from "$lib/assets/favicon.svg";
+	import navbarLogo from "$lib/assets/logo.svg";
+
+	import Button from "./Button.svelte";
 
 	let { children } = $props();
+
+	let btn = { A: ".", B: ".", C: ".", D: "." };
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<!------------------------------------------>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<title>Rolling Emporium</title>
+</svelte:head>
+
+<!------------------------------------------>
+
+<nav class="navbar-container">
+	<div class="navbar">
+		<img src={navbarLogo} alt="Logo" class="h-full" />
+
+		{#each Object.entries(btn) as [txt, hrf]}
+			<a href={hrf} class="h-full aspect-2/1">
+				<Button
+					lightColor="--color-slate-600"
+					darkColor="--color-slate-500"
+					text={txt}
+				/>
+			</a>
+		{/each}
+	</div>
+</nav>
+<div class="divider"></div>
+
 {@render children()}
+
+<footer class="footer-container">
+	<div class="footer">END</div>
+</footer>
+
+<!------------------------------------------>
+
+<style lang="postcss">
+	@import "./theme.css";
+
+	.navbar-container {
+		@apply w-full h-(--navbar-size) z-10
+		fixed top-0 left-0;
+	}
+	.navbar {
+		@apply w-full h-full p-5
+		flex flex-row justify-start items-center gap-5
+		bg-slate-400 dark:bg-slate-600 shadow-2xl;
+	}
+
+	.divider {
+		@apply h-(--navbar-size);
+	}
+
+	.footer-container {
+		@apply w-full h-(--navbar-size);
+	}
+	.footer {
+		@apply w-full h-full z-10
+		flex flex-row justify-center items-center
+		bg-slate-400 dark:bg-slate-600 shadow-2xl;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import Item from "./Item.svelte";
+
+	// Read JSON for items
+	let items = [
+		{ name: "A", cost: 3, img: "gallery/logo.svg" },
+		{ name: "B", cost: 3, img: "gallery/logo.svg" },
+		{ name: "C", cost: 3, img: "gallery/logo.svg" },
+		{ name: "D", cost: 3, img: "gallery/logo.svg" },
+		{ name: "E", cost: 3, img: "gallery/logo.svg" },
+		{ name: "F", cost: 3, img: "gallery/logo.svg" },
+		{ name: "G", cost: 3, img: "gallery/logo.svg" },
+		{ name: "H", cost: 3, img: "gallery/logo.svg" },
+		{ name: "I", cost: 3, img: "gallery/logo.svg" },
+	];
+</script>
+
+<!------------------------------------------>
+
+<section class="items-container">
+	{#each items as i}
+		<Item item={i} />
+	{/each}
+</section>
+
+<!------------------------------------------>
+
+<style lang="postcss">
+	@import "./theme.css";
+
+	.items-container {
+		@apply w-full h-full p-5
+		grid auto-rows-fr grid-cols-12 gap-5;
+	}
+</style>
