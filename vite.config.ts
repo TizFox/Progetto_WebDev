55ac9c1b6ebc46a01acd83f104975d0b304a0017
TizFox <tizbona04@gmail.com>
2026-03-19 10:07:26 +0100

    setup PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,32 @@
-import tailwindcss from '@tailwindcss/vite';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import tailwindcss from "@tailwindcss/vite";
+import { sveltekit } from "@sveltejs/kit/vite";
+import { SvelteKitPWA } from "@vite-pwa/sveltekit";
+import { defineConfig } from "vite";
 
-export default defineConfig({ plugins: [tailwindcss(), sveltekit()] });
+export default defineConfig({
+	plugins: [
+		tailwindcss(),
+		sveltekit(),
+		SvelteKitPWA({
+			registerType: "autoUpdate",
+			devOptions: { enabled: true },
+			manifest: {
+				name: "Rolling Emporium",
+				short_name: "RE",
+				theme_color: "#ffffff",
+				icons: [
+					{
+						src: "/pwa-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+					},
+					{
+						src: "/pwa-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+					},
+				],
+			},
+		}),
+	],
+});
